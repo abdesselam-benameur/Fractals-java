e9a131b69fddb65067134059b945d255bc68637f
Hakim IGUENI <hakim.igueni@etu.u-paris.fr>
2021-12-31 14:51:39 +0100

    Add program options

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.lang.invoke.StringConcatException;
 
 public class Main {
 //    MAX_ITER=1000; RADIUS=2.;
@@ -23,16 +24,19 @@ public class Main {
 //    }
 
     public static void main(String[] args) {
-        Getopt g = new Getopt("testprog", args, "w:h:a:b:c:d:m:");
+        Getopt g = new Getopt("testprog", args, "w:h:a:b:c:d:t:r:i:f:p:m");
         int c;
         String arg;
         int width = 1000;
         int height = 1000;
         int maxIter = 1000;
-        int xMin = -2;
-        int yMin = -2;
-        int xMax = 2;
-        int yMax = 2;
+        double re = -0.7269 ,im = 0.1889, pas = 0.0;
+        boolean mandelbrot = false;
+//        Complex constante = new Complex(0,0);
+        int xMin = -1;
+        int yMin = -1;
+        int xMax = 1;
+        int yMax = 1;
         while ((c = g.getopt()) != -1)
         {
             switch(c)
@@ -57,7 +61,7 @@ public class Main {
                         System.exit(1);
                     }
                     break;
-                case 'm':
+                case 't':
                     arg = g.getOptarg();
                     try {
                         maxIter = Integer.parseInt(arg);
@@ -80,7 +84,7 @@ public class Main {
                 case 'b':
                     arg = g.getOptarg();
                     try {
-                        yMax = Integer.parseInt(arg);
+                        xMax = Integer.parseInt(arg);
                     }
                     catch (NumberFormatException e) {
                         System.err.println("xMax must be an integer");
@@ -100,22 +104,57 @@ public class Main {
                 case 'd':
                     arg = g.getOptarg();
                     try {
-                        xMax = Integer.parseInt(arg);
+                        yMax = Integer.parseInt(arg);
                     }
                     catch (NumberFormatException e) {
-                        System.err.println("xMax must be an integer");
+                        System.err.println("yMax must be an integer");
                         System.exit(1);
                     }
                     break;
-//                case 'c':
-//                    arg = g.getOptarg();
-//                    System.out.print("You picked " + (char)c +
-//                            " with an argument of " +
-//                            ((arg != null) ? arg : "null") + "\n");
-//                    break;
+                case 'r':
+                    arg = g.getOptarg();
+                    try {
+                        re =  Double.parseDouble(arg);
+                        //constante.setRe(re);
+                    }
+                    catch (NumberFormatException e) {
+                        System.err.println("real must be an integer");
+                        System.exit(1);
+                    }
+                    break;
+                case 'i':
+                    arg = g.getOptarg();
+                    try {
+                        im = Double.parseDouble(arg);
+                        //constante.setIm(im);
+                        //System.out.println(constante.getIm());
+                    }
+                    catch (NumberFormatException e) {
+                        System.err.println("imaginary must be an integer");
+                        System.exit(1);
+                    }
+                    break;
+                case 'f':
+                    arg = g.getOptarg();
+                    File f = new File(arg);;
+                    break;
+                case 'p':
+                    arg = g.getOptarg();
+                    try {
+                        pas =  Double.parseDouble(arg);
+                    }
+                    catch (NumberFormatException e) {
+                        System.err.println("step must be an integer");
+                        System.exit(1);
+                    }
+                    break;
+                case 'm':
+                    mandelbrot = true;
+                    break;
 
                 case '?':
-                    System.out.println("Usage: main [-w width] [-h height]");
+                    System.out.println("Usage: main [-w width] [-h height] [-a xMin] [-b xMax] [-c yMin] [-d yMax] [-t maxIterations] [-m mandelbrot] "
+                            + "[-r real_part_constant] [-i imaginary_part_constant] [-f fileName] [-p pas]");
                     System.exit(1);
                     break; // getopt() already printed an error
                 //
@@ -124,9 +163,15 @@ public class Main {
             }
         }
 //
-        JuliaSetGenerator juliaSetGenerator = new JuliaSetGenerator(width, height, maxIter, -2,2,-2,2);
-//        juliaSetGenerator.generateMandelbrot();
-//        juliaSetGenerator.generate(-0.7269, 0.1889);
+//        JuliaSetGenerator juliaSetGenerator = new JuliaSetGenerator(width, height, maxIter, -2,2,-2,2);
+        if (mandelbrot) {
+            MandelbrotSetGenerator mandelbrotSetGenerator = new MandelbrotSetGenerator(width, height, maxIter, xMin, xMax, yMin, yMax);
+            mandelbrotSetGenerator.generate();
+        }
+        else {
+            JuliaSetGenerator juliaSetGenerator = new JuliaSetGenerator(width, height, maxIter, xMin, xMax, yMin, yMax);
+            juliaSetGenerator.generate(re, im);
 //        juliaSetGenerator.generate(0.4, 0.2);
+        }
     }
 }
